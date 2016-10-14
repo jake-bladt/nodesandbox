6c73115956058766cf84b4d8ce86125793d803d8
Jake Bladt <jake@jakebladt.com>
2016-10-14 18:48:53 -0400

    -legacy symlinks

--- boot.sh
+++ boot.sh
@@ -4,5 +4,4 @@ cd ~
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get install -y nodejs
-sudo apt-get install -y nodejs-legacy
 sudo apt-get install -y build-essential
