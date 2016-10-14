5b1c4351324db137a16d1ebe5d3c91b5df0f789b
Jake Bladt <jake@jakebladt.com>
2016-10-14 02:04:57 -0400

    127.0.0.1->0.0.0.0

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, World!\n');
 }).listen(8124, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8124');
+console.log('Server running at http://0.0.0.0:8124');
