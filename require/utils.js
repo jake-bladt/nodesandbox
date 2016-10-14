3d11dd74eb5fbb7b524483b1d0bda72b55ab570f
Jake Bladt <jake@jakebladt.com>
2016-10-14 11:30:45 -0400

    use js syntax

--- require/utils.js
+++ require/utils.js
@@ -2,5 +2,7 @@ var fs = require('fs');
 
 exports.listFiles = function() {
   var files = fs.readdirSync('.');
-  return files.map(f => return {fileName: f});
+  return files.map(
+    function(f) { return {fileName: f}; }
+  );
 };
