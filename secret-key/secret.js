8e126148989091e8b7f314eb9dbddd3315b266a1
Jake Bladt <jake@jakebladt.com>
2016-10-19 19:15:23 -0400

    +secret number

--- secret-key/secret.js
+++ secret-key/secret.js
@@ -1,3 +1,4 @@
 var fs = require("fs");
 var secrets = require("./.secret");
-console.log("Secret word is ", secrets.secretWord);
+console.log("Secret word is", secrets.secretWord);
+console.log("Secret number is", secrets.secretNumber);
