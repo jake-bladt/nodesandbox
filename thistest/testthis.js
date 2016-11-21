320ecadd10c3c06d5844116fd65a65c8da3a6f01
Jake Bladt <jake@jakebladt.com>
2016-11-21 01:18:00 -0500

    fix bracket panic

--- thistest/testthis.js
+++ thistest/testthis.js
@@ -1,14 +1,12 @@
 var higherFn = function(fName, lName) {
   return {
-    {
-      firstName: fName,
-      lastName: lName,
-      getFullName: function() {
-        return this.firstName + " " + this.LastName;
-      }
+    firstName: fName,
+    lastName: lName,
+    getFullName: function() {
+      return this.firstName + " " + this.LastName;
     }
   }
-}
+};
 
 var myObj = higherFn('Jake', 'Bladt');
 console.log(myObj.getFullName());
