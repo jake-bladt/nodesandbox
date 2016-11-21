bde45bfa01fc357f6a756254e1da9b9504179a0f
Jake Bladt <jake@jakebladt.com>
2016-11-21 01:15:55 -0500

    test use of this

--- thistest/testthis.js
+++ thistest/testthis.js
@@ -2,7 +2,13 @@ var higherFn = function(fName, lName) {
   return {
     {
       firstName: fName,
-      lastName: lName
+      lastName: lName,
+      getFullName: function() {
+        return this.firstName + " " + this.LastName;
+      }
     }
   }
 }
+
+var myObj = higherFn('Jake', 'Bladt');
+console.log(myObj.getFullName());
