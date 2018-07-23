b344799cb42ad1c97594891386278e4e6348a4c5
Jake Bladt <jake@jakebladt.com>
2018-07-22 22:27:49 -0400

    prove concept

--- mongoose0/index.js
+++ mongoose0/index.js
@@ -1,8 +1,24 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('http://localhost/kcourses')
+mongoose.connect('mongodb://localhost/kcourses')
 .then(() => {
     console.log("Successfully connected to the database");
+    const thingSchema = new mongoose.Schema({
+        name: String,
+        count: Number
+    });
+
+    const Thing = mongoose.model('thing', thingSchema);
+    const newThing = new Thing({ name: 'Bob', count: 42 });
+    newThing.save((err) => {
+      if(err) {
+        console.log('Thing failed to save.');
+        console.log(err);
+      } else {
+        console.log('Thing saved.');
+      }
+    });
+
   })
   .catch((err) => {
     console.log('Failed to connect to DB.');
