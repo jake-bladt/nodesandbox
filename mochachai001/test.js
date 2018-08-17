d7c32234b3fcf8ae9cc3236c486e53162484b929
Jake Bladt <jake@jakebladt.com>
2018-08-16 20:54:02 -0400

    +ES6 goodness

--- mochachai001/test.js
+++ mochachai001/test.js
@@ -1,12 +1,12 @@
 var assert = require('assert');
 
-describe('Basic Mocha String Test', function () {
+describe('Basic Mocha String Test', () => {
 
-    it('should return number of charachters in a string', function () {
-    assert.equal("Hello".length, 4);
+    it('should return number of charachters in a string', () => {
+    assert.equal("Hello".length, 5);
   });
 
-  it('should return first charachter of the string', function () {
+  it('should return first charachter of the string', () => {
     assert.equal("Hello".charAt(0), 'H');
   });
 });
